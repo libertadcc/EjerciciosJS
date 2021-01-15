19c1be80662cc778883ca2576432948c091f64e9
Libertad Chapinal <libertad.chapinal@estudiante.uam.es>
2021-01-15 10:02:58 +0100

    correct answer

--- ejercicio3/index.js
+++ ejercicio3/index.js
@@ -3,7 +3,7 @@
 const data = ['hola', 2, 5, 'adios'];
 
 // Determina cuál de las dos cadenas que hay en el array es mayor.
-console.log("'hola' es mayor que 'adios'", data[0]>data[3]);
+console.log('5 es mayor que 2', data[2]>data[1]);
 
 // Operaciones numéricas
 console.log('Suma', data[1]+data[2]);
