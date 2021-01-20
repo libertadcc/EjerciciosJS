dd7287f330e129ac3f1ddd389544e1a14b83f93d
Libertad Chapinal <libertad.chapinal@estudiante.uam.es>
2021-01-20 16:30:45 +0100

    Update main.js

--- ejercicio1/main.js
+++ ejercicio1/main.js
@@ -1,19 +1,23 @@
-'use strict';
+// base 16 * 23 cm
+// altura 13 cm
 
 const volumen = 16 * 23 * 13;
-console.log(volumen, 'cm3');
+console.log('volumen', volumen + ' cm3');
 
-const area = 2*(16 * 23 + 16 * 13 + 23 * 13);
-console.log(area, 'cm2');
+// A = al + 2ab
+const areaLateral = 23 *13;
+const areaBase = 16*23;
+const areaTotal = areaLateral + 2*areaBase;
+console.log('areaTotal', areaTotal + ' cm2');
 
 
-// Calculador de vólumenes y áreas generales
-const lado = 16;
-const ancho = 13;
-const base = 23;
+// Generalización
+const base = 12;
+const altura = 3;
+const ancho = 12;
 
-const volumen2 = lado * base * ancho;
-console.log(volumen2, 'cm3');
+const volumen2 = base * altura * ancho;
+console.log('volumen2', volumen2);
 
-const area2 = 2*(lado * base + lado * ancho + base * ancho);
-console.log(area2, 'cm2');
+const area2 = base * altura + 2 * base * ancho;
+console.log('area2', area2);
