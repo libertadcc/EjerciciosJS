0df4eb01d84544a369619359dbec186275a96897
Libertad Chapinal <libertad.chapinal@estudiante.uam.es>
2021-01-27 08:30:23 +0100

    Update main.js

--- ejercicio5/main.js
+++ ejercicio5/main.js
@@ -11,18 +11,3 @@ if (dni < 0 || dni > 99999999) {
   const letterSelected = letras[dni%23];
   alert('Tu letra del DNI es '+ letterSelected);
 };
-
-const scores = [4, 2, 7, 8, 6, 7, 9, 1, 2, 6, 7];
-
-// Creamos una variable fuera del bucle para que no se sobrescriba en cada iteración
-// En esta variable iremos sumando cada una de las puntuaciones
-let acc = 0;
-
-// La i empieza en 0 porque el índice de los arrays empieza en 0 también
-for (let i = 0; i < scores.length; i++) {
-  acc += scores[i];
-  // Sumamos a `acc` el valor actual del array en cada iteración del bucle
-  // acc += arr[i] es igual a acc = acc + arr[i]
-}
-
-console.log('La puntuación final es ' + acc);
